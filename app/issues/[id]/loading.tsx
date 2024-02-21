db6ba360224342e11a2da7d93e0090986610ad69
GopalVia2117 <gopals2117@gmail.com>
2024-02-21 21:13:09 +0530

    Refractor: Organize import statements

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,15 +1,12 @@
-import IssueStatusBadge from '@/app/components/IssueStatusBadge';
-import { Heading, Flex, Card, Box } from '@radix-ui/themes';
-import React from 'react';
+import { Box, Card, Flex } from '@radix-ui/themes';
 import Skeleton from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
-
 
 const LoadingIssueDetailPage = () => {
   return (
     <Box className="max-w-xl">
-      <Heading><Skeleton/></Heading>
-          <Flex gap={'3'} my='2'>
+          <Skeleton/>
+          <Flex gap='3' my='2'>
               <Skeleton width="5rem"/>
               <Skeleton width="8rem"/>
             </Flex>
